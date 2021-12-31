db107a095cd785a9cb5095914b742acd3d319535
Takanori Ishikawa <ishikawa_takanori@mac.com>
2021-12-31 10:19:24 +0900

    Parsing parentheses

--- src/lib.rs
+++ src/lib.rs
@@ -26,43 +26,48 @@ pub enum Expr {
 }
 
 pub fn parser() -> impl Parser<char, Expr, Error = Simple<char>> {
-    let int = text::int(10)
-        .map(|s: String| Expr::Num(s.parse().unwrap()))
-        .padded();
+    let expr = recursive(|expr| {
+        let int = text::int(10)
+            .map(|s: String| Expr::Num(s.parse().unwrap()))
+            .padded();
 
-    let atom = int;
+        let atom = int.or(expr.delimited_by('(', ')'));
 
-    let op = |c| just(c).padded();
+        let op = |c| just(c).padded();
 
-    // unary: "-"
-    let unary = op('-')
-        .repeated()
-        .then(atom)
-        .foldr(|_op, rhs| Expr::Neg(Box::new(rhs)));
+        // unary: "-"
+        let unary = op('-')
+            .repeated()
+            .then(atom)
+            .foldr(|_op, rhs| Expr::Neg(Box::new(rhs)));
 
-    // binary: "*", "/"
-    let product = unary
-        .then(
-            op('*')
-                .to(Expr::Mul as fn(_, _) -> _)
-                .or(op('/').to(Expr::Div as fn(_, _) -> _))
-                .then(unary)
-                .repeated(),
-        )
-        .foldl(|lhs, (op, rhs)| op(Box::new(lhs), Box::new(rhs)));
+        // binary: "*", "/"
+        let product = unary
+            .clone()
+            .then(
+                op('*')
+                    .to(Expr::Mul as fn(_, _) -> _)
+                    .or(op('/').to(Expr::Div as fn(_, _) -> _))
+                    .then(unary)
+                    .repeated(),
+            )
+            .foldl(|lhs, (op, rhs)| op(Box::new(lhs), Box::new(rhs)));
 
-    // binary: "+", "-"
-    let sum = product
-        .then(
-            op('+')
-                .to(Expr::Add as fn(_, _) -> _)
-                .or(op('-').to(Expr::Sub as fn(_, _) -> _))
-                .then(product)
-                .repeated(),
-        )
-        .foldl(|lhs, (op, rhs)| op(Box::new(lhs), Box::new(rhs)));
+        // binary: "+", "-"
+        let sum = product
+            .clone()
+            .then(
+                op('+')
+                    .to(Expr::Add as fn(_, _) -> _)
+                    .or(op('-').to(Expr::Sub as fn(_, _) -> _))
+                    .then(product)
+                    .repeated(),
+            )
+            .foldl(|lhs, (op, rhs)| op(Box::new(lhs), Box::new(rhs)));
+        sum.padded()
+    });
 
-    sum.then_ignore(end())
+    expr.then_ignore(end())
 }
 
 pub fn eval(expr: &Expr) -> Result<f64, String> {
