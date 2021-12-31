51782026f8d51b9d81e32396f4c9b6a3ab9a08e2
Takanori Ishikawa <ishikawa_takanori@mac.com>
2021-12-31 14:41:25 +0900

    Split token lexing into a separate compilation stage (#3)

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,31 @@ fn main() {
     let filepath = env::args().nth(1).expect("filename");
     let src = fs::read_to_string(filepath).expect("Read source code");
 
-    match paty::parser().parse(src) {
-        Ok(ast) => match paty::eval(&ast) {
-            Ok(output) => println!("{}", output),
-            Err(err) => println!("Evaluation error: {}", err),
-        },
-        Err(err) => err.into_iter().for_each(|e| println!("Parse error: {}", e)),
-    }
+    let tokens = match paty::lexer().parse(src) {
+        Err(err) => {
+            err.into_iter()
+                .for_each(|e| eprintln!("Syntax error: {}", e));
+            std::process::exit(exitcode::DATAERR);
+        }
+        Ok(tokens) => tokens,
+    };
+    //println!("tokens = {:?}", tokens);
+
+    let ast = match paty::parser().parse(tokens) {
+        Err(err) => {
+            err.into_iter()
+                .for_each(|e| eprintln!("Parse error: {}", e));
+            std::process::exit(exitcode::DATAERR);
+        }
+        Ok(ast) => ast,
+    };
+    //println!("ast = {:?}", ast);
+
+    match paty::eval(&ast) {
+        Ok(output) => println!("{}", output),
+        Err(err) => {
+            eprintln!("Evaluation error: {}", err);
+            std::process::exit(exitcode::UNAVAILABLE);
+        }
+    };
 }
