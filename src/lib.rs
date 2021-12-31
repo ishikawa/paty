9c1d17a537aed0e12c0f9a5d9f454e1c63efd130
Takanori Ishikawa <ishikawa_takanori@mac.com>
2021-12-31 10:15:17 +0900

    Parsing binary operators

--- src/lib.rs
+++ src/lib.rs
@@ -34,12 +34,35 @@ pub fn parser() -> impl Parser<char, Expr, Error = Simple<char>> {
 
     let op = |c| just(c).padded();
 
+    // unary: "-"
     let unary = op('-')
         .repeated()
         .then(atom)
         .foldr(|_op, rhs| Expr::Neg(Box::new(rhs)));
 
-    unary.then_ignore(end())
+    // binary: "*", "/"
+    let product = unary
+        .then(
+            op('*')
+                .to(Expr::Mul as fn(_, _) -> _)
+                .or(op('/').to(Expr::Div as fn(_, _) -> _))
+                .then(unary)
+                .repeated(),
+        )
+        .foldl(|lhs, (op, rhs)| op(Box::new(lhs), Box::new(rhs)));
+
+    // binary: "+", "-"
+    let sum = product
+        .then(
+            op('+')
+                .to(Expr::Add as fn(_, _) -> _)
+                .or(op('-').to(Expr::Sub as fn(_, _) -> _))
+                .then(product)
+                .repeated(),
+        )
+        .foldl(|lhs, (op, rhs)| op(Box::new(lhs), Box::new(rhs)));
+
+    sum.then_ignore(end())
 }
 
 pub fn eval(expr: &Expr) -> Result<f64, String> {
